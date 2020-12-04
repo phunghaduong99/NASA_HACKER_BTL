f88ac9a779858ab12824a796e69968d7cb866f2f
Tyrell <tyrell@trueplus.vn>
2020-12-04 23:33:16 +0700

    Resolve conflict

--- config/config.php
+++ config/config.php
@@ -1,16 +1,20 @@
 <?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "testlogin");
 
 /** Configuration Variables **/
 
-define ('DEVELOPMENT_ENVIRONMENT',false);
+define ('DEVELOPMENT_ENVIRONMENT', false);
 
 
-define('DB_NAME', 'framework');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'Phunghaduong@1612');
-define('DB_HOST', 'localhost');
+// define('DB_NAME', 'framework');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'Phunghaduong@1612');
+// define('DB_HOST', 'localhost');
 
-define('BASE_PATH','http://localhost/framework');
+// define('BASE_PATH','http://localhost/framework');
 
 
 define('PAGINATE_LIMIT', '5');
