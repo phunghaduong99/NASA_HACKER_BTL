d1095b4000e2440c97d919bd0a237b5c1eb6bd91
dinhthuyha <54362507+dinhthuyha@users.noreply.github.com>
2020-12-06 09:32:44 +0700

    ha 06/12

---
+++ view/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
+
+<body>
+<div class="header">
+    <div class="left">
+        <div class="img">
+            <img class="img" src="images/logo.png" alt="">
+        </div>
+        <div class="home">
+            <a href="#"> | OUTstagram</a>
+        </div>
+
+    </div>
+    <div class="right">
+        <div class="login">
+            <div class="login-user">
+                <a href="">Login</a>
+            </div>
+
+        </div>
+        <div class="register">
+            <a  href="">Register</a></div>
+    </div>
+
+</div>
+
+<div class="container ">
+    <div class="form">
+        <div class="logo">
+            <div class="img">
+                <img src="images/logo.png" alt="">
+            </div>
+
+            <h1 class="title">OutSta</h1>
+        </div>
+        <form>
+            <div class="email ">
+                <label class="title">E-mail Address</label> <br>
+                <input class="form-input" type="text" name="" required="re"><br>
+            </div>
+            <div class="pass">
+                <label class="title">Password</label> <br>
+                <input class="form-input" type="password" name="" id=""><br>
+            </div>
+            <div class="check title">
+                <input class="check" type="checkbox" name="" value=""> Rememeber me <br>
+            </div>
+            <div class="">
+                <button class="btn-hover color-1  " type="submit" value="Login">Login
+            </div>
+            <div class="forgot title">
+                <a class="forgot" href="">Forgot Your Password</a>
+            </div>
+
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ view/register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="register.css">
+    <title>Register</title>
+</head>
+
+<body>
+<div class="header">
+    <div class="left">
+        <div class="img">
+            <img class="img" src="images/logo.png" alt="">
+        </div>
+        <div class="home">
+            <a href="#"> | OUTstagram</a>
+        </div>
+
+    </div>
+    <div class="right">
+        <div class="login">
+            <a href="">Login</a>
+        </div>
+        <div class="register">
+            <a href="">Register</a></div>
+    </div>
+
+</div>
+<div class="container">
+    <div class="logo">
+        <div class="img">
+            <img src="images/logo.png" alt="">
+        </div>
+
+        <h1 class="title">OutSta</h1>
+    </div>
+    <form>
+        <div class="name ">
+            <label class="title">Name</label> <br>
+            <input class="form-input" type="text" name="" id=""><br>
+        </div>
+        <div class="email">
+            <label class="title">E-mail Address</label> <br>
+            <input class="form-input" type="text" name="" id=""><br>
+        </div>
+        <div class="username">
+            <label class="title">Username</label> <br>
+            <input class="form-input" type="text" name="" id=""><br>
+        </div>
+        <div class="password">
+            <label class="title">Password</label> <br>
+            <input class="form-input" type="text" name="" id=""><br>
+        </div>
+        <div class="confirm">
+            <label class="title">Confirm Password</label> <br>
+            <input class="form-input" type="text" name="" id=""><br>
+        </div>
+        <div>
+            <button class="btn-hover color-1  " type="submit" value="Login">Register
+        </div>
+    </form>
+</div>
+
+
+
+
+</body>
+
+</html>
