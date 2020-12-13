d5ee9736d15a43cf77466449a157d6bc1380da7f
dinhthuyha <54362507+dinhthuyha@users.noreply.github.com>
2020-12-13 09:41:47 +0700

    Update edit.php

--- application/views/users/edit.php
+++ application/views/users/edit.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/edit_profile.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Edit Profile</title>
-</head>
 
-<body>
-<?php include ("")?>
 <div class="container ">
     <div class="form">
         <div class="logo">
@@ -53,6 +44,3 @@
 </div>
 
 
-</body>
-
-</html>
