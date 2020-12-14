d9053f5f5442d6514a0c06ddf9e90f9e7b3e7ec4
phunghaduong99 <phunghaduong99@gmail.com>
2020-12-14 11:16:52 +0700

    cap nhat view

--- application/views/users/header.php
+++ application/views/users/header.php
@@ -13,27 +13,31 @@
 
 <body>
 <div class="header">
-    <div class="left">
-        <div class="img"> <img class="img" src="/public/img/logo.png" alt=""></div>
-        <div class="center">
-            <p>|</p>
-        </div>
-        <a class="home btn btn-2 color-green" href="">OUTstagram</a>
-    </div>
-    <div class="center-search">
-        <form class="search">
-            <input type="text" name="search" placeholder="Search...">
-        </form>
-    </div>
-    <div class="right">
-        <div class=" right dropdown">
-            <div class="dropbtn">
-                <p>lalalisa</p>
-                <img src="/public/img/icon.jpg" alt="">
+    <div class="header-container">
+        <div class="left">
+            <div class="img"> <img class="img" src="/public/img/logo.png" alt=""></div>
+            <div class="center">
+                <p>|</p>
             </div>
-            <div class="dropdown-content">
-                <a href="#">Log Out</a>
+            <a class="home btn btn-2 color-green" href="">OUTstagram</a>
+        </div>
+        <div class="center-search">
+            <form class="search">
+                <input type="text" name="search" placeholder="Search...">
+            </form>
+        </div>
+        <div class="right">
+            <div class=" right dropdown">
+                <div class="dropbtn">
+                    <p>lalalisa</p>
+                    <img src="/public/img/icon.jpg" alt="">
+                </div>
+                <div class="dropdown-content">
+                    <a href="#">Log Out</a>
+                </div>
             </div>
         </div>
     </div>
+
 </div>
+
